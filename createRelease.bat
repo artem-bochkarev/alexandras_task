f27a8aacb6f8e43047a48cd8b22f04f19de22499
Artem Bochkarev <artem.bochkarev@gmail.com>
2013-03-19 19:52:04 +0400

    Bat file for rlease creation added

---
+++ createRelease.bat
@@ -0,0 +1,7 @@
+mkdir Product
+copy /B "Release\Policlinic.exe" Product\
+copy "Policlinic\qt_ru.qm" Product\
+copy "Policlinic\translate_ru.qm" Product\
+copy %QTDIR32%\bin\QtGui4.dll Product\
+copy %QTDIR32%\bin\QtCore4.dll Product\
+copy docsBAD.txt Product\Doctors.txt
